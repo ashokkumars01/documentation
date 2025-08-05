986e50c7c875834f4e3c7905578c9301699f2600
Ashok Kumar Shivashankar (MS/ETX1) <iash1kor@bosch.com>
2025-08-05 17:50:21 +0530

    Update

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,10 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-# -- Project information
-project = 'MyProject'
-version = ''  # short version
-release = ''  # full version
+project = 'Your Project'
+author = 'Your Name'
+release = '1.0.0'
 
-# -- General configuration
 extensions = [
     'sphinx_rtd_theme',
     'sphinx_multiversion',
@@ -16,11 +14,10 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-# -- HTML output
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# -- sphinx-multiversion config
-smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'  # only tags like v1.0.0
+# Sphinx-multiversion config
+smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'
 smv_branch_whitelist = r'^master$'
 smv_remote_whitelist = r'^origin$'
