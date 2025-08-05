02a49241989709e55258dda60d68fd9cec696073
Ashok Kumar Shivashankar (MS/ETX1) <iash1kor@bosch.com>
2025-08-05 17:53:55 +0530

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,18 +6,18 @@ project = 'Your Project'
 author = 'Your Name'
 release = '1.0.0'
 
+
+master_doc = 'index'
+
 extensions = [
     'sphinx_rtd_theme',
     'sphinx_multiversion',
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
-
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
-# Sphinx-multiversion config
+# Versioning config
 smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'
 smv_branch_whitelist = r'^master$'
 smv_remote_whitelist = r'^origin$'
+
